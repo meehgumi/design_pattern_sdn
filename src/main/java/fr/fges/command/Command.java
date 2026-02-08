253fdac642652d951b4f2956c2e2eeebd5f1fb6a
unknown <2023680546@lacatholille.fr>
2026-02-08 05:23:25 +0100

    feat: add undo support in interface

--- src/main/java/fr/fges/command/Command.java
+++ src/main/java/fr/fges/command/Command.java
@@ -4,3 +4,5 @@ public interface Command {
     void execute();    // La méthode qui lancera l'action
     String getLabel(); // Le texte qui s'affichera dans le menu
 }
+// Permet d'éviter d'implémenter undo() dans toutes les classes
+    default void undo() {}
