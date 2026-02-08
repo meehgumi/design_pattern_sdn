02bd1cbac7b03f9886d23c0ad84977f7bca43192
unknown <2023680546@lacatholille.fr>
2026-02-08 05:48:51 +0100

    feat: implementation of undo for DeleteGame

--- src/main/java/fr/fges/command/DeleteCommand.java
+++ src/main/java/fr/fges/command/DeleteCommand.java
@@ -1,15 +1,28 @@
 package fr.fges.command;
+import fr.fges.BoardGame;
 import fr.fges.GameCollection;
 import java.util.Scanner;
 
 public class DeleteCommand implements Command {
+    private BoardGame deletedGame;
     public String getLabel() { return "Delete a game"; }
 
     public void execute() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Title of game to delete: ");
         String title = sc.nextLine();
-        GameCollection.removeGame(title);
-        System.out.println("Done!");
+        this.deletedGame = GameCollection.removeGame(title);
+        if (this.deletedGame != null) {
+            System.out.println("Jeu supprime !");
+        } else {
+            System.out.println("Jeu non trouve.");
+        }
+    }
+
+    public void undo() {
+        if (this.deletedGame != null) {
+            GameCollection.addGame(this.deletedGame);
+            System.out.println("Undo: " + this.deletedGame.title() + " restaure.");
+        }
     }
 }
